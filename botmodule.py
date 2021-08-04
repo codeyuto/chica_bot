68c340a71f2785101792dfd02c04e918969f3233
codeyuto <takosuke29@gmail.com>
2021-08-04 22:01:55 +0900

    add question

--- botmodule.py
+++ botmodule.py
@@ -1,6 +1,17 @@
 from slackbot.bot import respond_to
 from slackbot.bot import listen_to
- 
+import random
+
+def choose_question():
+    question={"ハローキティのリボンの色は？":"赤",
+    "Pythonの名前の由来となったコメディ番組は？":"モンティ・パイソン",
+    "Rubyの開発者の名前は？":"まつもとゆきひろ"
+    }
+
+    q,a=random.choice(list.(question.items()))
+
+    return  q,a
+
 #メンションに対する応答
 @respond_to("ハロー")
 def greeting_1(message):
@@ -15,3 +26,14 @@ def greeting_2(message):
 @respond_to("辛い")
 def greeting_3(message):
     message.reply("ココア入れてあげるから、これ飲んで早く寝なさいな")
+
+@respond_to("問題出して")
+def question(message):
+    q,a=choose_question()
+    message.reply("それじゃあ問題。"+q)
+    @respond_to(r'.+')
+    if(message.body["text"]==a):
+        message.reply("正解！")
+    else:
+        message.reply("残念。正解は"+q+"でした。")
+    
