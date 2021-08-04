27577c597ed89a7afefe202f621ac78ed02857bf
codeyuto <takosuke29@gmail.com>
2021-08-04 22:13:25 +0900

    modified something

--- botmodule.py
+++ botmodule.py
@@ -2,16 +2,6 @@ from slackbot.bot import respond_to
 from slackbot.bot import listen_to
 import random
 
-def choose_question():
-    question={"ハローキティのリボンの色は？":"赤",
-    "Pythonの名前の由来となったコメディ番組は？":"モンティ・パイソン",
-    "Rubyの開発者の名前は？":"まつもとゆきひろ"
-    }
-
-    q,a=random.choice(list.(question.items()))
-
-    return  q,a
-
 #メンションに対する応答
 @respond_to("ハロー")
 def greeting_1(message):
